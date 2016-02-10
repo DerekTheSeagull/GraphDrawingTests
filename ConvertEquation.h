087993eb3b8fa7f58ea108d4e8c407464aab99f2
Josh <joshuasmithnow@gmail.com>
2016-02-10 15:15:53 +0000

    "ConvertEquation.h" 1.0 is now complete, only adds "*"s right now, more will be added once I have tested the symbolicc++ library more.

--- ConvertEquation.h
+++ ConvertEquation.h
@@ -12,14 +12,15 @@ using namespace std;
 
 const int iArrayHeight = 6;
 
-bool InArray(pair <const char*, pair<string, string>> aArray[iArrayHeight][1], const char* cTarget);
+pair <bool, int> InArray(pair <const char*, pair<string, string>> aArray[iArrayHeight][1], const char* cTarget);
 
-string ConvertEquation(string sEquation)
-{
-    pair <const char*, pair<string, string>> aConditionalConversions[iArrayHeight][1];
+bool InString(string sString, const char* cTarget);
 
+string ConvertEquation(string sEquation) {
+    string sFinalEquation;
     const char* csEquation = sEquation.c_str();
 
+    pair <const char*, pair<string, string>> aConditionalConversions[iArrayHeight][1];
     aConditionalConversions[0][0] = make_pair("x", make_pair("0123456789(", "*x"));
     aConditionalConversions[1][0] = make_pair("y", make_pair("0123456789(", "*y"));
     aConditionalConversions[2][0] = make_pair("s", make_pair("0123456789(", "*s"));
@@ -27,27 +28,44 @@ string ConvertEquation(string sEquation)
     aConditionalConversions[4][0] = make_pair("t", make_pair("0123456789(", "*t"));
     aConditionalConversions[5][0] = make_pair("(", make_pair("0123456789(", "*("));
 
-    for (int i = 0; i < sEquation.length(); ++i)
-    {
+
+    for (int i = 0; i < sEquation.length(); ++i) {
         const char* ccCurrentChar = &csEquation[i];
 
-        cout << ccCurrentChar;
-        if (i > 0 && InArray(aConditionalConversions, ccCurrentChar))
-        {
-            cout << "We did it reddit!";
+        pair <bool, int> inArray;
+        bool inString;
+
+        inArray = InArray(aConditionalConversions, ccCurrentChar);
+
+        inString = aConditionalConversions[inArray.second][0].second.first.find(sEquation[i-1]);
+
+        if (i > 0 && inArray.first && inString) {
+            sFinalEquation += aConditionalConversions[inArray.second][0].second.second;
+        }
+        else {
+            sFinalEquation += sEquation[i];
         }
     }
 
 
-    return "0";
+    return sFinalEquation;
 }
 
-bool InArray(pair <const char*, pair<string, string>> aArray[iArrayHeight][1], const char* cTarget)
-{
-    for (int i = 0; i < iArrayHeight; ++i)
-    {
-        if(strcmp(cTarget, aArray[i][0].first) == 0)
-        {
+pair <bool, int> InArray(pair <const char*, pair<string, string>> aArray[iArrayHeight][1], const char* cTarget) {
+    for (int i = 0; i < iArrayHeight; ++i) {
+        if(strcmp(cTarget, aArray[i][0].first) == 0) {
+            return make_pair(true, i);
+        }
+    }
+
+    return make_pair(false, 0);
+}
+
+bool InString(string sString, const char* cTarget) {
+    for (int i = 0; i < sString.length(); ++i) {
+        char cCurrentChar = sString[i];
+
+        if (strcmp(cTarget, &cCurrentChar) == 0) {
             return true;
         }
     }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <c++/array>
-#include "ConvertEquation.h"
 
 using namespace std;
 
 int main() {
-    ConvertEquation("2x");
+
 
     return 0;
 }
