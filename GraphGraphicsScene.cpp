369a701cc8a1d27582dd4db3f6f4cadcac74a565
DerekTheSeagull <joshuasmithnow@gmail.com>
2016-05-09 22:01:01 +0000

    v0.1.1 Started commenting of code (this really should have been done much sooner)

--- GraphGraphicsScene.cpp
+++ GraphGraphicsScene.cpp
@@ -33,6 +33,10 @@ GraphGraphicsScene::GraphGraphicsScene(int w, int h) {
 void GraphGraphicsScene::DrawGrid(int scale) {
     int i = 0;
 
+
+    //Loops through all values of x that are divisible by the scale starting at the smallest
+    //visible x value that is divisible by the scale
+    //Then draws lines for the grid
     for (int x = iMinX_ - (iMinX_ % scale); x <= iMaxX_; x += scale) {
         GridLines_[i]->setLine(x, iMinY_, x, iMaxY_);
         if (GridLines_[i]->scene() == this) {
@@ -45,6 +49,10 @@ void GraphGraphicsScene::DrawGrid(int scale) {
 
     i = iLinesToStore_ - 1;
 
+
+    //Loops through all values of y that are divisible by the scale starting at the smallest
+    //visible y value that is divisible by the scale
+    //Then draws lines for the grid
     for (int y = iMinY_ - (iMinY_ % scale); y <= iMaxY_; y += scale) {
         GridLines_[i]->setLine(iMinX_, y, iMaxX_, y);
         if (GridLines_[i]->scene() == this) {
@@ -58,29 +66,29 @@ void GraphGraphicsScene::DrawGrid(int scale) {
 }
 
 void GraphGraphicsScene::DrawAxis() {
-    if (iMinX_ > 0) {
+    if (iMinX_ > 0) { //draws x axis if view showing scene to the right of origin
         yAxis_.setLine(iMinX_, iMinY_, iMinX_, iMaxY_);
         removeItem(&yAxis_);
         addItem(&yAxis_);
-    } else if (iMaxX_ < 0) {
+    } else if (iMaxX_ < 0) { //draws x axis if view showing scene to the left of origin
         yAxis_.setLine(iMaxX_ - 1, iMinY_, iMaxX_ - 1, iMaxY_);
         removeItem(&yAxis_);
         addItem(&yAxis_);
-    } else if (iMinX_ < 0 < iMaxX_) {
+    } else if (iMinX_ < 0 < iMaxX_) { //draws x axis if x = 0 is currently on screen
         yAxis_.setLine(0, iMinY_, 0, iMaxY_);
         removeItem(&yAxis_);
         addItem(&yAxis_);
     }
 
-    if (iMinY_ > 0) {
+    if (iMinY_ > 0) { //draws y axis if view showing scene below origin
         xAxis_.setLine(iMinX_, iMinY_, iMaxX_, iMinY_);
         removeItem(&xAxis_);
         addItem(&xAxis_);
-    } else if (iMaxY_ < 0) {
+    } else if (iMaxY_ < 0) { //draws y axis if view showing scene above origin
         xAxis_.setLine(iMinX_, iMaxY_ - 1, iMaxX_, iMaxY_ - 1);
         removeItem(&xAxis_);
         addItem(&xAxis_);
-    } else if (iMinY_ < 0 < iMaxY_) {
+    } else if (iMinY_ < 0 < iMaxY_) { //draws y axis if y = 0 is currently on screen
         xAxis_.setLine(iMinX_, 0, iMaxX_, 0);
         removeItem(&xAxis_);
         addItem(&xAxis_);
