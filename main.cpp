624385babcf19569cf3c9c9e8ede799550287006
Josh <joshuasmithnow@gmail.com>
2016-06-23 20:33:25 +0100

    v0.2 Added -DrawGraph function now implemented, now just requires the equation reading function TODO -add buttons and functions to main window class that allow the user to control the scene -implement "muparser" to allow values to be calculated for the drawing of the graph -auto-size window to best size on boot and allow user to specify a size to use

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 #include <QtWidgets/qdesktopwidget.h>
 #include "ConvertEquation.h"
 #include "MainWindow.h"
+#include "ReadEquation.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
-    QApplication app (argc, argv);
-    MainWindow mw;
-    mw.show();
-    app.exec();
+    //QApplication app (argc, argv);
+    //MainWindow mw;
+    //mw.show();
+    //app.exec();
+
+    trig_function<double>();
 
     return 0;
 }
